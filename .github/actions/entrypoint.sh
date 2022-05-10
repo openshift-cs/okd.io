b6b02a46c8c15e42bf702381d7718f983aa01820
Brian Innes <brianinnes@users.noreply.github.com>
2022-05-10 19:03:21 +0100

    update tooling versions

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -3,7 +3,7 @@
 cd "${GITHUB_WORKSPACE}"
 
 # Python Dependencies
-pip --no-cache-dir install git+https://github.com/linkchecker/linkchecker@v10.0.1#egg=linkchecker
+pip --no-cache-dir install git+https://github.com/linkchecker/linkchecker@v10.1.0#egg=linkchecker
 # NodeJS Dependencies
 npm ci
 
